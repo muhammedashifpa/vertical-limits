ddaee44a5f09218158188eea132f6a02c59cad60
muhammed ashif <ashifmohammed231@gmail.com>
2021-12-13 00:06:48 +0530

    updates

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,39 +1,80 @@
-// $(document).ready(function(){
-//     $('.your-class').slick({
-//         dots: true,
-//         infinite: false,
-//         speed: 300,
-//         slidesToShow: 4,
-//         slidesToScroll: 4,
-//         responsive: [
-//           {
-//             breakpoint: 1024,
-//             settings: {
-//               slidesToShow: 3,
-//               slidesToScroll: 3,
-//               infinite: true,
-//               dots: true
-//             }
-//           },
-//           {
-//             breakpoint: 600,
-//             settings: {
-//               slidesToShow: 2,
-//               slidesToScroll: 2
-//             }
-//           },
-//           {
-//             breakpoint: 480,
-//             settings: {
-//               slidesToShow: 1,
-//               slidesToScroll: 1
-//             }
-//           }
-//           // You can unslick at a given breakpoint now by adding:
-//           // settings: "unslick"
-//           // instead of a settings object
-//         ]
-//       });
-//   });
+$(document).on('ready', function () {
 
-$('.single-item').slick();
+
+    $(window).resize(function() {
+        if ($(window).width() > 992) {
+            $('.course-list').removeClass('course-slick');
+            $('.course-list').addClass('course-grid');
+        } else {
+            $('.course-list').removeClass('course-grid');
+            $('.course-list').addClass('course-slick');
+        }
+    }).resize();
+
+    $(".regular").slick({
+    //   dots: true,
+    //   infinite: true,
+      slidesToShow: 3,
+      slidesToScroll: 3,
+      infinite: false,
+      speed: 300,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }]
+      
+    });
+    $(".course-slick").slick({
+    //   dots: true,
+    //   infinite: true,
+      slidesToShow: 2,
+      slidesToScroll: 2,
+      infinite: false,
+      speed: 300,
+      responsive: [
+        
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }]
+      
+    });
+
+    // $(".variable").slick({
+    //   dots: true,
+    //   infinite: true,
+    //   variableWidth: true
+    // });
+    $(".lazy").slick({
+      lazyLoad: 'ondemand', // ondemand progressive anticipated
+      infinite: true
+    });
+  });
